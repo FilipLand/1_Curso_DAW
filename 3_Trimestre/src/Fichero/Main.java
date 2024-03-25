d15207b18fe5e02989484bfd07670e32644cea41
FilipLand <filip9038@gmail.com>
2024-03-25 10:33:49 +0100

    mejorado

--- 3_Trimestre/src/Fichero/Main.java
+++ 3_Trimestre/src/Fichero/Main.java
@@ -5,24 +5,35 @@ import java.io.File;
 public class Main {
     static String linea = "==============================================================";
     public static void main(String[]args){
-
-    System.out.println(linea);
-    String archivoRelativo = "Fichero.txt";
-    File ficheroRel = new File(archivoRelativo);
-    System.out.println("Relativo " + ficheroRel);
-    System.out.println(linea);
     
-    System.out.println(linea);
-    String archivoAbsoluto = "Programacion\\3_Trimestre\\src\\Fichero\\Fichero.txt";
-    File ficheroAbs = new File(archivoAbsoluto);
-    System.out.println("Absoluto " + ficheroAbs);
-    System.out.println(linea);
-
-    System.out.println(linea);
-    String otroArchivo = "C:\\Users\\alumno";
-    File ficheroOtro = new File(otroArchivo);
-    System.out.println("Otro " + ficheroOtro);
-    System.out.println(linea);
+    Relativo();
+    Absoluto();
+    Otro();
+    }
     
+    public static void Relativo(){
+
+        System.out.println(linea);
+        String archivoRelativo = "Fichero.txt";
+        File ficheroRel = new File(archivoRelativo);
+        System.out.println("Relativo " + ficheroRel);
+        System.out.println(linea);
+    }
+    public static void Absoluto(){
+
+        System.out.println(linea);
+        String archivoAbsoluto = "Programacion\\3_Trimestre\\src\\Fichero\\Fichero.txt";
+        File ficheroAbs = new File(archivoAbsoluto);
+        System.out.println("Absoluto " + ficheroAbs);
+        System.out.println(linea);
+    }
+    public static void Otro(){
+
+        System.out.println(linea);
+        String otroArchivo = "C:\\Users\\alumno";
+        File ficheroOtro = new File(otroArchivo);
+        System.out.println("Otro " + ficheroOtro);
+        System.out.println(linea);
+
     }
 }
