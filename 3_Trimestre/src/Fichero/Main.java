4fd2893be379dda49bf0445473428e81a52fe545
FilipLand <filip9038@gmail.com>
2024-03-25 10:12:16 +0100

    file

--- 3_Trimestre/src/Fichero/Main.java
+++ 3_Trimestre/src/Fichero/Main.java
@@ -3,25 +3,25 @@ package Fichero;
 import java.io.File;
 
 public class Main {
-    static String linea = "============================";
+    static String linea = "==============================================================";
     public static void main(String[]args){
 
     System.out.println(linea);
     String archivoRelativo = "Fichero.txt";
     File ficheroRel = new File(archivoRelativo);
-    System.out.println(ficheroRel);
+    System.out.println("Relativo " + ficheroRel);
     System.out.println(linea);
     
     System.out.println(linea);
     String archivoAbsoluto = "Programacion\\3_Trimestre\\src\\Fichero\\Fichero.txt";
     File ficheroAbs = new File(archivoAbsoluto);
-    System.out.println(ficheroAbs);
+    System.out.println("Absoluto " + ficheroAbs);
     System.out.println(linea);
 
     System.out.println(linea);
     String otroArchivo = "C:\\Users\\alumno";
     File ficheroOtro = new File(otroArchivo);
-    System.out.println(ficheroOtro);
+    System.out.println("Otro " + ficheroOtro);
     System.out.println(linea);
     
     }
