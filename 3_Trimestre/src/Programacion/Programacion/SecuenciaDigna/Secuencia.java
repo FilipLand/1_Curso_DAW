e560b9779b0666f7aaf56bb5cf1689cd3d5ba454
FilipLand <filip9038@gmail.com>
2024-04-19 09:47:06 +0200

    Secuencia

--- 3_Trimestre/src/Programacion/Programacion/SecuenciaDigna/Secuencia.java
+++ 3_Trimestre/src/Programacion/Programacion/SecuenciaDigna/Secuencia.java
@@ -4,13 +4,26 @@ public class Secuencia {
     static java.util.Scanner in;
 
     public static void casoDePrueba() {
+
         int cantNum = in.nextInt();
         int[] cadena = new int[cantNum];
+        
         for (int i = 0; i < cadena.length; i++) {
             String numCad = in.nextLine();
+            String[] partes = numCad.split(" ");
+            
+            for (int j = 0; j < partes.length; j++) {
+                int valor1 = Integer.parseInt(partes[j + 1]);
+                int valor2 = Integer.parseInt(partes[j]);
+                
+                if (Math.abs(valor2) > (Math.abs(valor1))) {
+                    if(){
 
-            cadena[i] = 1;
-
+                    };
+                } else if () {
+                    
+                }
+            }
         }
     } // casoDePrueba
 
