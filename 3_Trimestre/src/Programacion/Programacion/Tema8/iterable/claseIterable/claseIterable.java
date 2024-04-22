4e23a6083b1e27dcd01e51911e9d6a35f1f56b4d
FilipLand <filip9038@gmail.com>
2024-04-22 10:20:11 +0200

    iterator

---
+++ 3_Trimestre/src/Programacion/Programacion/Tema8/iterable/claseIterable/claseIterable.java
@@ -0,0 +1,14 @@
+/**Crea la clase alumno con los siguientes atributos:
+ *  nombre, nia, edad, curso y nota media. 
+ *  Crea además una clase que implemente la interfaz Iterable
+ *  y que permita recorrer una lista de alumnos. Crea un
+ *  programa que genere una lista de alumnos y los imprima
+ *  por pantalla. A continuación, recorre esa lista con un
+ *  iterador, imprimiendo los alumnos, uno a uno por pantalla. 
+ * */
+
+package Programacion.Tema8.iterable.claseIterable;
+
+public class claseIterable {
+
+}
