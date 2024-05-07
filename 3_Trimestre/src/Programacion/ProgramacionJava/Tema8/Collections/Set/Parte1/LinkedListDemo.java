0507dfae1094a2d84b42ffe2df2075159ac34015
FilipLand <filip9038@gmail.com>
2024-05-07 10:29:24 +0200

    Set

---
+++ 3_Trimestre/src/Programacion/ProgramacionJava/Tema8/Collections/Set/Parte1/LinkedListDemo.java
@@ -0,0 +1,21 @@
+package ProgramacionJava.Tema8.Collections.Set.Parte1;
+
+public class LinkedListDemo {
+    public static void main(String[] args) {
+        hashset();
+        LinkedHashSet();
+        Treset();
+    }
+
+    public static void hashset() {
+
+    }
+
+    public static void LinkedHashSet() {
+
+    }
+
+    public static void Treset() {
+
+    }
+}
