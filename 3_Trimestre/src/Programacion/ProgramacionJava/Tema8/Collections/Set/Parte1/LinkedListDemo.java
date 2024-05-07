9ff15be1c4b66fd66f1b866812d410737a441d2c
FilipLand <filip9038@gmail.com>
2024-05-07 10:43:23 +0200

    Set

--- 3_Trimestre/src/Programacion/ProgramacionJava/Tema8/Collections/Set/Parte1/LinkedListDemo.java
+++ 3_Trimestre/src/Programacion/ProgramacionJava/Tema8/Collections/Set/Parte1/LinkedListDemo.java
@@ -1,13 +1,24 @@
 package ProgramacionJava.Tema8.Collections.Set.Parte1;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class LinkedListDemo {
     public static void main(String[] args) {
         hashset();
-        LinkedHashSet();
-        Treset();
+        // LinkedHashSet();
+        // Treset();
     }
 
     public static void hashset() {
+        Set<Alumno> hashSet = new HashSet<>();
+
+        hashSet.add(new Alumno("Franek", "Mlynarczyk", 10));
+        System.out.println(hashSet);
+        hashSet.add(new Alumno("Artur", "Mlynarczyk", 12));
+        System.out.println(hashSet);
+        hashSet.add(new Alumno("Kevin", "Frank", 12));
+        System.out.println(hashSet);
 
     }
 
