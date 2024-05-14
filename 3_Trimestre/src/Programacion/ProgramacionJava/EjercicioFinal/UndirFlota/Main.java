3ca1633a9617db904a50e67ec63627da4395a8dc
FilipLand <filip9038@gmail.com>
2024-05-14 10:47:16 +0200

    trabajo

---
+++ 3_Trimestre/src/Programacion/ProgramacionJava/EjercicioFinal/UndirFlota/Main.java
@@ -0,0 +1,13 @@
+package ProgramacionJava.EjercicioFinal.UndirFlota;
+
+public class Main {
+    String agua = "O";
+    String explotadoVacio = "X";
+    String explotadoLleno = "D";
+    static String barco = "◼";
+
+    public static void main(String[] args) {
+        System.out.println(barco);
+
+    }
+}
